f1696549d5545b730fe3edcd0ac0e1a35c6626a3
raul <raulgarcia@icsoftware.es>
2017-06-01 13:13:38 +0200

    documentado

--- SQLPDF/SQLPDF/SQLPDF.cs
+++ SQLPDF/SQLPDF/SQLPDF.cs
@@ -30,6 +30,32 @@ namespace SQLPDF
 		protected float linea;
 		protected float y;
 		protected int n_tickets_pag;
+		
+		/// <summary>
+		/// Constructor: coge el json parseado y lo guarda como diccionario tambien coge del json:
+		/// offset: la distancia desde el origen y de la pagina hasta donde se pone la cabecera
+		/// y: la distancia desde el origne y de la pagina hasta donde se pone la informacion
+		/// linea: distancia de interlineado
+		/// n_tickets_pag: numero de lineas que tiene cada pagina sin contar la cabecera
+		/// destatico: diccionario donde para cada texto estatico se guarda su estilo
+		/// </summary>
+		/// <param name="pdfjson">path donde esta el.json que se parsea</param>
+		/// <example>ejemplo de json:
+		/// {"offset":{"y":75},
+		///   "n_tickets_pag":20,
+    	///	  "estatico":[
+        ///		{"x":50,"fonttype":"ArialBlack","fontsize":20,"texto":"espectaculo"},
+        ///		{"x":150,"fonttype":"ArialBlack","fontsize":20,"texto":"abonado"},
+        ///		{"x":270,"fonttype":"ArialBlack","fontsize":20,"texto":"devoluciones"},
+        ///		{"x":1000,"fonttype":"ArialBlack","fontsize":20,"texto":"importe"}      
+    	///	   ],
+    	///	   "offsetres":{"linea":"20","y":100},
+    	///	   "espectaculo":{"x":50,"fonttype":"verdana","fontsize":17},
+    	///	   "abonado":{"x":150,"fonttype":"verdana","fontsize":17},
+		///	   "devoluciones":{"x":270,"fonttype":"verdana","fontsize":17},
+    	///	   "importe":{"x":1000,"fonttype":"verdana","fontsize":17}
+		///	}
+		/// </example>
 		public SQLPDF(String pdfjson){
 			 config = new Dictionary<string, Object>();
              StreamReader r = new StreamReader(pdfjson);
@@ -57,58 +83,70 @@ namespace SQLPDF
              
 			
 		}
+		
+		 
+		
+		/// <summary>
+		/// Esta funcion busca la fuente de un texto estatico que pone nombre
+		/// </summary>
+		/// <param name="nombre"> texto del campo estatico del que queremos saber la fuente</param>
+		/// <returns></returns>
 		public System.Drawing.Font encontrar_font(string nombre){
 			Estilo est = destatico[nombre];
 			return new System.Drawing.Font(est.fonttype, est.fontsize);
 			
 		}
+		/// <summary>
+		/// Esta funcion busca el campo nombre en el json parseado y dentro de este busca la fuente
+		/// </summary>
+		/// <param name="nombre">nombre del campo del json del que queremos saber su fuente</param>
+		/// <returns></returns>
 		public System.Drawing.Font encontrar_font_row(string nombre){
 			Dictionary<string,Object> estilo = (Dictionary<string,Object>)config[nombre];
 			return new System.Drawing.Font(estilo["fonttype"].ToString(), (float)Convert.ToDouble(estilo["fontsize"].ToString()));
 			
 		}
+		
+		/// <summary>
+		/// Esta funcion busca que x tiene el campo estatico con texto nombre
+		/// </summary>
+		/// <param name="nombre">nombre del campo del json del que queremos saber la x</param>
+		/// <returns></returns>
 		public float encontrar_x_row(string nombre){
 			Dictionary<string,Object> estilo = (Dictionary<string,Object>)config[nombre];
 			return (float)Convert.ToDouble(estilo["x"].ToString());
 		}
-		
+		/// <summary>
+		/// Esta funcion nos dice en que x tiene que ir el texto estatico nombre
+		/// </summary>
+		/// <param name="nombre">texto del campo estatico del que queremos saber su x</param>
+		/// <returns></returns>
 		public float encontrar_x(string nombre){
 			Estilo est = destatico[nombre];
 			return est.x;
 		}
+		
+		/// <summary>
+		/// La funcion que hace la exportacion a pdf
+		/// </summary>
+		/// <param name="dt">Tabla que vamos a printar en pdf</param>
+		/// <param name="path">path donde se guardara el pdf final</param>
 		public void ExportToPdf(DataTable dt,string path)
-   		{      
-			Document document = new Document(PageSize.A4.Rotate());
-    		
+   		{
+			/*Abrimos un fichero de tipo pdf*/			
+			Document document = new Document(PageSize.A4.Rotate());    		
     		PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(path, FileMode.Create));
     		document.Open();
-            iTextSharp.text.Font font5 = iTextSharp.text.FontFactory.GetFont(FontFactory.HELVETICA, 5);
-
-    		PdfPTable table = new PdfPTable(dt.Columns.Count);
-    		PdfPRow row = null;
-    		/*float[] widths = new float[] { 4f, 4f, 4f, 4f };
-
-    		table.SetWidths(widths);*/
-    		
-
-    		table.WidthPercentage = 100;
-    		int iCol = 0;
-    		string colname = "";
-    		PdfPCell cell = new PdfPCell(new Phrase("Products"));
-
-    		cell.Colspan = dt.Columns.Count;
-    		System.Drawing.Image gif = System.Drawing.Image.FromFile("0001.png");
-    		
+    		/*Cargamos como imagen la que tenemos como plantilla*/
+    		System.Drawing.Image gif = System.Drawing.Image.FromFile("0001.png");    		
 			Graphics graphics = Graphics.FromImage(gif);
 			printar_cabecera(graphics,dt);
 			SolidBrush drawBrush = new SolidBrush(Color.Black);
-    		foreach (DataColumn c in dt.Columns)
-    		{
-    			
-    			graphics.DrawString(c.ColumnName, encontrar_font(c.ColumnName),drawBrush ,encontrar_x(c.ColumnName),offset);
-    			//table.AddCell(new Phrase(c.ColumnName, encontrar_font(c.ColumnName)));
-    		}
 			int lineaact=0;
+			/*Para cada la printamos en la x que se nos pasa el json y como y la linea en la que 
+			 * esta por el interlineado mas la distancia desde donde comienza a 
+			 * printarse la informacion
+			 */
     		foreach (DataRow r in dt.Rows)
     		{
         		if (dt.Rows.Count > 0)
@@ -116,10 +154,12 @@ namespace SQLPDF
         			int i=0;
         			foreach (DataColumn c in dt.Columns)
     				{
-        				//table.AddCell(new Phrase(r[i].ToString(), encontrar_font_row(c.ColumnName)));
         				graphics.DrawString(r[i].ToString(), encontrar_font_row(c.ColumnName),drawBrush ,encontrar_x_row(c.ColumnName),(lineaact*linea)+y);
         				i++;
         			}
+        			/*si es la ultima linea guardamos la imagen y cogemos como nueva imagen
+        			 * la de la plantilla y le printamos la cabecera.Sino decimos que la linea actual para la proxima iteración será está mas uno
+        			 */
         			if(lineaact==n_tickets_pag){
         				lineaact=0;
         				document.Add(iTextSharp.text.Image.GetInstance(gif,System.Drawing.Imaging.ImageFormat.Gif));
@@ -135,11 +175,15 @@ namespace SQLPDF
     		} 
 			
 			
-    		
+    		/*añadimos la ultima imagen y cerramos el documento*/
 			document.Add(iTextSharp.text.Image.GetInstance(gif,System.Drawing.Imaging.ImageFormat.Gif));
-    		//document.Add(table);
         	document.Close();
 		}
+		/// <summary>
+		/// Printa la cabecera en la imagen
+		/// </summary>
+		/// <param name="graphics">el componente para printar en la imagen</param>
+		/// <param name="dt">el DataTable con los datos</param>
 		public void printar_cabecera(Graphics graphics, DataTable dt){
 			SolidBrush drawBrush = new SolidBrush(Color.Black);
     		foreach (DataColumn c in dt.Columns)
