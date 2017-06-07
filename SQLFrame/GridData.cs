acefc4bece659bfa17dfe8919f1664214985f3f7
raul <raulgarcia@icsoftware.es>
2017-06-07 12:33:25 +0200

    comentado totalmente

--- SQLFrame/GridData.cs
+++ SQLFrame/GridData.cs
@@ -19,6 +19,16 @@ namespace SQLFrame
 	public class GridData
 	{
 		public static DataTable queryresult;
+		/// <summary>
+		/// Es la funcion principal
+		/// </summary>
+		/// <param name="input">DataTable original</param>
+		/// <param name="formato">no utilizado de momento</param>
+		/// <param name="filepdf">no utilizado de momento</param>
+		/// <param name="fileconfig">json que se utiliza para ver de que va
+		/// riables y para que variables se quieren los subtotales </param>
+		/// <returns>Devuelve el Datatable con los subttotales ya metidos.
+		/// </returns>
 		public static DataTable printar(DataTable input,string formato,string filepdf,string fileconfig){
 			queryresult=input;
 			Core c = new Core(formato,filepdf,fileconfig);
